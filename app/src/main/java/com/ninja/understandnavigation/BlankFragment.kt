6cde911058577773b4df838facabb486c2b53666
kavie <kavita.manikumar@gmail.com>
2023-03-07 21:07:27 +0530

    Initial commit

---
+++ app/src/main/java/com/ninja/understandnavigation/BlankFragment.kt
@@ -0,0 +1,52 @@
+package com.ninja.understandnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nextButton = view.findViewById<Button>(R.id.goNext)
+        nextButton?.setOnClickListener {
+            //Handle navigation without Safeargs
+           // findNavController().navigate(R.id.action_blankFragment_to_otherFragment,null)
+
+            //using safety-args
+            val action = BlankFragmentDirections.actionBlankFragmentToOtherFragment()
+            it.findNavController().navigate(action)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/ninja/understandnavigation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ninja.understandnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      /*  val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController*/
+
+
+    }
+}
